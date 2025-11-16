bf2d557d8b0ff1ab146baa5a6544e8a5506cd921
Ilya <141645849+Dredarker@users.noreply.github.com>
2025-11-16 16:10:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 i = 0;
 setInterval(() => {
-    i += 0.1;
+    i += 1;
     document.body.style["background-position-x"] = `${i}px`;
-}, 0);
+}, 1000);
+
+function pressKeyToRun(runCode, key) {
+    function runCode2() {
+        if (event.key === key) {
+            runCode();
+            document.removeEventListener('keydown', runCode2);
+        }
+    };
+    document.addEventListener('keydown', runCode2);
+};
+
+TMPcode = () => {
+    
+};
+
+pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {TMPcode()}, "a")}, "b")}, "ArrowRight")}, "ArrowLeft")}, "ArrowRight")}, "ArrowLeft")}, "ArrowDown")}, "ArrowDown")}, "ArrowUp")}, "ArrowUp");
