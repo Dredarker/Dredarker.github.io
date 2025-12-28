06eea3347273b8809c27f3cbe751c4745d8a4c73
Ilya <141645849+Dredarker@users.noreply.github.com>
2025-12-29 00:14:47 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ setInterval(() => {
     document.body.style["background-position-x"] = `${i}px`;
 }, 65);
 
+fetch("https://raw.githubusercontent.com/Dredarker/Dredarker.github.io/refs/heads/main/news.txt")
+.then(result => result.text())
+.then(body => document.querySelector("body > div > div:nth-child(2) > p").outerHTML = body)
+
+
+
 function pressKeyToRun(runCode, key) {
     function runCode2() {
         if (event.key === key) {
@@ -13,9 +19,7 @@ function pressKeyToRun(runCode, key) {
     };
     document.addEventListener('keydown', runCode2);
 };
-
+pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {TMPcode()}, "a")}, "b")}, "ArrowRight")}, "ArrowLeft")}, "ArrowRight")}, "ArrowLeft")}, "ArrowDown")}, "ArrowDown")}, "ArrowUp")}, "ArrowUp");
 TMPcode = () => {
     alert("Нету посхалки :(")
 };
-
-pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {pressKeyToRun(() => {TMPcode()}, "a")}, "b")}, "ArrowRight")}, "ArrowLeft")}, "ArrowRight")}, "ArrowLeft")}, "ArrowDown")}, "ArrowDown")}, "ArrowUp")}, "ArrowUp");
